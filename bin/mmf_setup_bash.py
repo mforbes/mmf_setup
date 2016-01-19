106123f0453193ba7165cce2b5a7e1d98dc1f70e
Michael McNeil Forbes <michael.forbes+github@gmail.com>
2016-01-18 17:37:00 -0700

    API: Start branch 0.1.4.  Cleanup after small code review. - Depend on nbstripout 0.2.0 which is now on PyPI - Don't worry about adding ~/.hgrc to HGRCPATH if it does not exist.   It is not a problem.

--- bin/mmf_setup_bash.py
+++ bin/mmf_setup_bash.py
@@ -1,36 +1,36 @@
 #!/bin/env python
 from optparse import OptionParser
-from os.path import exists, abspath, expanduser
+from os.path import exists
 
 import mmf_setup
 
+# List of (variable, value, filename)
+# Will print a statement like the following iff filename is None or exists:
+#
+#    export variable=value
+#
 VARIABLES = [
     ('MMF_SETUP', mmf_setup.MMF_SETUP, mmf_setup.MMF_SETUP),
-    ('HGRCPATH', '${HGRCPATH:-~/.hgrc}', expanduser('~/.hgrc')),
-    ('HGRCPATH', '${{HGRCPATH}}:{HGRC}'.format(
-        HGRC=mmf_setup.HGRC), mmf_setup.HGRC),
+    ('HGRCPATH', ':'.join(['${HGRCPATH:-~/.hgrc}', mmf_setup.HGRC]), mmf_setup.HGRC),
 ]
 
 
 def run(debug=False):
-    environment_string = []
-    for var, val, filename in VARIABLES:
-        if debug:
-            print("# processing {}={} for file '{}'"
-                  .format(var, val, filename))
+    env = []
+    for var, value, filename in VARIABLES:
         if not filename or exists(filename):
-            environment_string.append(
-                'export {var}="{val}"'.format(var=var, val=val))
-        if debug and filename and not exists(filename):
-            print("# processing {} failed:\n   no file '{}'"
-                  .format(var, filename))
-    environment_string = "\n".join(environment_string)
-    print(environment_string)
+            env.append('export {var}="{value}"'.format(var=var, value=value))
+        elif debug:
+            print("# processing {}={} failed:\n   no file '{}'"
+                  .format(var, value, filename))
+
+    print("\n".join(env))
+
 
 parser = OptionParser()
 parser.add_option("-d", "--debug",
                   action="store_true", dest="debug", default=False,
-                  help="show debugging information")
+                  help="debug missing files")
 
 if __name__ == '__main__':
     (options, args) = parser.parse_args()
