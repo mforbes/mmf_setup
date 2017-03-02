e14775c4f69e7e579eca1956c7c9d904150d390c
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2017-03-01 17:37:00 -0800

    REL: 0.1.8 Bump revision number and updated changelog.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open(os.path.join(_HERE, 'README.rst')) as _f:
 
 
 setup(name=NAME,
-      version='0.1.8dev',
+      version='0.1.8',
       packages=find_packages(exclude=['tests']),
 
       setup_requires=setup_requires,
