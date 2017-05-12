a5eb6314b30253c258657bfdd36be44ac90c5535
Michael McNeil Forbes <michael.forbes+github@gmail.com>
2017-05-11 19:13:15 -0700

    BUG: Added test demonstrating issue #17 (no way to reference HGROOT). - Removed old test for issue #12 which is no longer relevant.

--- mmf_setup/test/test_issue_12.py
+++
@@ -1,9 +0,0 @@
-"""Regression test for Issue 12: enable_nbextension."""
-
-from mmf_setup.notebook_configuration import ExtensionManager
-
-
-def test_issue_12():
-    manager = ExtensionManager()
-    manager.enable('noop')
-    manager.disable('noop')
---
+++ mmf_setup/tests/test_issue_17.py
@@ -0,0 +1,9 @@
+"""Regression test for issue #17: add HGROOT variable."""
+
+import mmf_setup
+import subprocess
+
+
+def test_issue_17():
+    mmf_setup.nbinit(hgroot=True, debug=True)
+    assert mmf_setup.HGROOT == subprocess.check_output(['hg', 'root']).strip()
