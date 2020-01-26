54bb91358ca6514c2ab75f0140c572dc7eb12745
Michael McNeil Forbes <michael.forbes+github@gmail.com>
2020-01-26 11:06:32 -0800

    BRN: Start working on 0.3.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ LONG_DESCRIPTION = "\n".join([
 
     
 setup(name=NAME,
-      version='0.3.0',
+      version='0.3.1dev',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       py_modules=[splitext(basename(_path))[0] for _path in glob('src/*.py')],
